98ad39f4e0478253369cf1db19f58ec59c9573d5
Azor <azor@ubuntu.com>
2022-03-13 12:11:04 -0400

    I have added 2 rows of calc

--- azor_calc.py
+++ azor_calc.py
@@ -9,17 +9,23 @@ class Calc:
     def __init__(self) -> None:
         #window settings
         self.root = Tk()
-        self.root.geometry("300x450")
+        self.root.geometry("260x450")
         self.root.resizable(0,0)
         self.root.title("Azor_calc")
         self.background = Label(self.root, bg="white").place(x=0,y=0,relwidth=1,relheight=1)
 
         #entry
-        self.entry = Entry(self.root, justify="right", font=("Terminal", 35), bg="#2d4870", fg="white").place(x=0, y=0, width=300, height=100)
+        self.entry = Entry(self.root, justify="right", font=("Terminal", 25), bg="#2d4870", fg="white").place(x=0, y=0, width=260, height=100)
 
         #buttons
         ac = Button(self.root, text="AC", font=("Terminal", 20), bg="white", fg="black").place(x=0, y=100, width=70, height=70)
-
+        x2 = Button(self.root, text="x²", font=("Terminal", 20), bg="white", fg="black").place(x=65, y=100, width=70, height=70)
+        koren = Button(self.root, text="√", font=("Terminal", 20), bg="white", fg="black").place(x=130, y=100, width=70, height=70)
+        divide = Button(self.root, text="÷", font=("Terminal", 20), bg="#ff7700", fg="white").place(x=195, y=100, width=70, height=70)
+        num7 = Button(self.root, text="7", font=("Time", 20), bg="white", fg="black").place(x=0, y=168, width=68, height=70)
+        num8 = Button(self.root, text="8", font=("Time", 20), bg="white", fg="black").place(x=65, y=168, width=68, height=70)
+        num9 = Button(self.root, text="9", font=("Time", 20), bg="white", fg="black").place(x=130, y=168, width=68, height=70)
+        multiply = Button(self.root, text="×", font=("Terminal", 20), bg="#ff7700", fg="white").place(x=195, y=168, width=68, height=70)
         self.root.mainloop()
 
 user = Calc()
