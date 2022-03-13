1f338d6968ecc47c4c4314f4edc4ad366c1ba689
Azor <azor@ubuntu.com>
2022-03-13 11:24:56 -0400

    I have changed size of window and added ac button

--- azor_calc.py
+++ azor_calc.py
@@ -9,16 +9,16 @@ class Calc:
     def __init__(self) -> None:
         #window settings
         self.root = Tk()
-        self.root.geometry("400x400")
+        self.root.geometry("300x450")
         self.root.resizable(0,0)
         self.root.title("Azor_calc")
         self.background = Label(self.root, bg="white").place(x=0,y=0,relwidth=1,relheight=1)
 
         #entry
-        # self.entry = Entry(self.root,  bg="#E7E6E6")
-        # self.entry.place(x=0,y=0,width=350,height=35)
-        self.entry = Entry(self.root, justify="right", font=("Terminal", 35), bg="#20569e", fg="white").place(x=0, y=0, width=400, height=100)
+        self.entry = Entry(self.root, justify="right", font=("Terminal", 35), bg="#2d4870", fg="white").place(x=0, y=0, width=300, height=100)
 
+        #buttons
+        ac = Button(self.root, text="AC", font=("Terminal", 20), bg="white", fg="black").place(x=0, y=100, width=70, height=70)
 
         self.root.mainloop()
 
