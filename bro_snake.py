77f684fbe150777b7f22ef4a2580c28d4441eeea
Azor <azor@ubuntu.com>
2022-03-16 10:48:27 -0400

    I have writed basic of this game in this code. Window, class, func

--- bro_snake.py
+++ bro_snake.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from turtle import window_height
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
@@ -33,3 +34,30 @@ def check_collisions():
 
 def game_over():
     pass
+
+#settings of interface
+interface = Tk()
+interface.title("Bro_snake")
+interface.resizable(0,0)
+
+#score_label
+score = 0
+score_label = Label(interface, text="Score:{}".format(score), font=("consolas", 40))
+score_label.pack()
+
+#game arena
+arena = Canvas(interface, width=GAME_WIDTH, height=GAME_HEIGHT, bg=BACKGROUND_COLOR)
+arena.pack()
+
+#interface geometry
+interface.update()
+screen_width = interface.winfo_screenwidth()
+screen_height = interface.winfo_screenheight()
+interface_height = interface.winfo_height()
+interface_width = interface.winfo_width()
+
+x = int((screen_width/2) - (interface_width/2))
+y = int((screen_height/2) - (interface_height/2))
+
+interface.geometry(f"{interface_width}x{interface_height}+{x}+{y}")
+interface.mainloop()
